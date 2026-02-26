aa57f6df9c356d31821953092e5c0506d2a1f2e4
Ryan <rpeters1428@gmail.com>
2026-02-25 18:07:58 -0800

    fix(pornhoarder): restore listings and fix ajax post payload

--- plugin.video.cumination/resources/lib/sites/pornhoarder.py
+++ plugin.video.cumination/resources/lib/sites/pornhoarder.py
@@ -17,6 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import re
+import requests
 from resources.lib import utils
 from resources.lib.adultsite import AdultSite
 from six.moves import urllib_parse
@@ -28,6 +29,49 @@ site = AdultSite(
     "pornhoarder.png",
     "pornhoarder",
 )
+
+ph_headers = {
+    "Origin": site.url[:-1],
+    "User-Agent": utils.USER_AGENT,
+    "X-Requested-With": "XMLHttpRequest",
+    "Cookie": "pornhoarder_settings=0---0---1---0",
+}
+
+
+def _extract_srcset_url(value):
+    if not value:
+        return ""
+    first = value.split(",")[0].strip()
+    return first.split(" ")[0].strip()
+
+
+def _extract_search_term(url):
+    match = re.search(r"[?&]search=([^&]+)", url)
+    if not match:
+        return ""
+    return urllib_parse.unquote(match.group(1))
+
+
+def Createdata(page=1, search=""):
+    return [
+        ("search", search),
+        ("sort", "0"),
+        ("date", "0"),
+        ("servers[]", "21"),
+        ("servers[]", "40"),
+        ("servers[]", "12"),
+        ("servers[]", "35"),
+        ("servers[]", "25"),
+        ("servers[]", "41"),
+        ("servers[]", "46"),
+        ("servers[]", "17"),
+        ("servers[]", "44"),
+        ("servers[]", "42"),
+        ("servers[]", "43"),
+        ("servers[]", "29"),
+        ("author", "0"),
+        ("page", page),
+    ]
 
 
 @site.register(default_mode=True)
@@ -62,77 +106,99 @@ def Main():
 
 @site.register()
 def List(url, page=1, section=None):
-    search = ""
-    referer = url
-    if "ajax_search.php" in url:
-        ajax_url = url
-        post_data = {"page": page, "search": search}
-        referer = site.url
-    else:
-        # If it's a search URL or other page, try to use AJAX
-        ajax_url = site.url + "ajax_search.php"
-        search_match = re.findall(r"search=([^&]+)", url)
-        search = urllib_parse.unquote(search_match[0]) if search_match else ""
-        
-        author_match = re.findall(r"/(?:porn-star|porn-studio)/([^/]+)/", url)
-        author = author_match[0] if author_match else "0"
-        
-        post_data = {"page": page, "search": search}
-        if author != "0":
-            # We don't have the numeric author ID easily, but the site seems to 
-            # use Referer to filter AJAX results when search is empty.
-            pass
+    search = "" if isinstance(url, str) and url.startswith("https://") else url
+    siteurl = section if section else site.url
 
-    headers = {
-        "Referer": referer,
-        "X-Requested-With": "XMLHttpRequest"
-    }
-    
-    listhtml = utils.postHtml(ajax_url, data=post_data, headers=headers)
+    if isinstance(url, str) and url.startswith("https://"):
+        if "ajax_search.php" in url:
+            ajax_url = url
+            referer = siteurl
+        else:
+            ajax_url = siteurl + "ajax_search.php"
+            referer = url
+            search = _extract_search_term(url)
+    else:
+        ajax_url = siteurl + "ajax_search.php"
+        referer = siteurl
+
+    headers = dict(ph_headers)
+    headers["Referer"] = referer
+
+    listhtml = utils.postHtml(ajax_url, form_data=Createdata(page, search), headers=headers)
     if not listhtml:
-        # Fallback to GET if POST failed
-        listhtml = utils.getHtml(url, site.url)
-        
+        fallback_url = url
+        if not isinstance(url, str) or not url.startswith("https://"):
+            fallback_url = siteurl + "search/?search={}".format(urllib_parse.quote(search))
+        listhtml = utils.getHtml(fallback_url)
+
     soup = utils.parse_html(listhtml)
-    items = soup.select(".video, article")
-    for item in items:
-        link = item.select_one("a.video-link, a[href*='/pornvideo/']")
-        if not link:
-            continue
-            
+    links = soup.select("article a[href], a[href]")
+    for link in links:
         videourl = utils.safe_get_attr(link, "href", default="")
-        if not videourl:
+        if not videourl or "/video" not in videourl:
             continue
-            
-        videourl = urllib_parse.urljoin(site.url, videourl)
-        
-        img_tag = item.select_one(".video-image.primary, img")
-        img = utils.get_thumbnail(img_tag)
+
+        videourl = urllib_parse.urljoin(siteurl, videourl)
+        img = ""
+        for img_tag in link.select("img"):
+            src = utils.safe_get_attr(
+                img_tag,
+                "data-src",
+                ["data-original", "data-lazy", "data-srcset", "srcset", "src"],
+            )
+            src = _extract_srcset_url(src)
+            alt = utils.safe_get_attr(img_tag, "alt", default="").lower()
+            if "logo" in src.lower() or "logo" in alt:
+                continue
+            img = src
+            if img:
+                break
         if img and img.startswith("/"):
-            img = urllib_parse.urljoin(site.url, img)
-            
-        name = utils.cleantext(utils.safe_get_text(item.select_one("h1"), default=""))
+            img = urllib_parse.urljoin(siteurl, img)
+
+        name = utils.cleantext(utils.safe_get_text(link.select_one("h1"), default=""))
         if not name:
             name = utils.cleantext(utils.safe_get_attr(link, "title", default=""))
         if not name:
-            h2 = item.select_one("h2")
+            h2 = link.select_one("h2")
             name = utils.cleantext(h2.get_text()) if h2 else ""
         if not name:
             name = utils.cleantext(utils.safe_get_text(link))
-            
-        duration = utils.safe_get_text(item.select_one(".video-length"), default="")
-        
+        if not name:
+            continue
+
+        duration = utils.safe_get_text(link.select_one(".length"), default="")
+        if not duration:
+            duration = utils.safe_get_text(link.select_one(".video-length"), default="")
+
         site.add_download_link(
             name, videourl, "Playvid", img, name, duration=duration
         )
 
-    if len(items) >= 20:
+    next_page = ""
+    next_button = soup.select_one(".next .pagination-button[data-page]")
+    if next_button:
+        next_page = utils.safe_get_attr(next_button, "data-page", default="")
+    if not next_page:
+        next_link = soup.select_one(".pagination .next a[href]")
+        if next_link:
+            next_page = utils.safe_get_attr(next_link, "data-page", default="").strip()
+            if not next_page:
+                next_page = utils.safe_get_text(next_link, default="").strip()
+            if not next_page:
+                href = utils.safe_get_attr(next_link, "href", default="")
+                match = re.search(r"/page/(\d+)/", href)
+                if match:
+                    next_page = match.group(1)
+
+    if next_page:
         site.add_dir(
-            "Next Page...",
+            "Next Page ({})".format(next_page),
             url,
             "List",
             site.img_next,
-            page=int(page) + 1
+            page=int(next_page),
+            section=siteurl,
         )
 
     utils.eod()
@@ -164,22 +230,36 @@ def Playvid(url, name, download=None):
 
 @site.register()
 def Categories(url):
-    html = utils.getHtml(url, site.url)
+    html = utils.getHtml(url)
     soup = utils.parse_html(html)
-    for item in soup.select(".video.category"):
-        link = item.select_one("a.video-link")
-        if not link: continue
-        
+    for item in soup.select(".video.category, article"):
+        link = item.select_one("a[href]")
+        if not link:
+            continue
+
         caturl = utils.safe_get_attr(link, "href", default="")
-        caturl = urllib_parse.urljoin(site.url, caturl)
-        
+        search_term = _extract_search_term(caturl)
+        if not search_term:
+            continue
+
         name = utils.cleantext(utils.safe_get_text(item.select_one("h2")))
         img_tag = item.select_one("img")
         img = utils.get_thumbnail(img_tag)
         if img and img.startswith("/"):
             img = urllib_parse.urljoin(site.url, img)
-            
-        site.add_dir(name, caturl, "List", img)
+
+        site.add_dir(name, search_term, "List", img)
+
+    next_link = soup.select_one(".pagination .next a[href]")
+    if next_link:
+        page_number = utils.safe_get_text(next_link, default="").strip()
+        if page_number:
+            site.add_dir(
+                "Next Page ({})".format(page_number),
+                utils.safe_get_attr(next_link, "href", default=""),
+                "Categories",
+                site.img_next,
+            )
     utils.eod()
 
 
@@ -225,5 +305,8 @@ def Search(url, keyword=None):
     if not keyword:
         site.search_dir(url, "Search")
     else:
-        search_url = url.format(urllib_parse.quote(keyword))
-        List(search_url)
+        if "{0}" in url:
+            search_url = url.format(urllib_parse.quote(keyword))
+            List(search_url)
+        else:
+            List(keyword, page=1)
